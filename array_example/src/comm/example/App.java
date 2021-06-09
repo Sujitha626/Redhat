e54eb6724cf062e8aebb752bde23766eafe0bd3c
001ZMY744 <001ZMY744@LAPTOP-EGSTOHKO>
2021-06-09 16:27:34 -0700

    Queue

--- array_example/src/comm/example/App.java
+++ array_example/src/comm/example/App.java
@@ -3,77 +3,41 @@ package comm.example;
 import java.util.Scanner;
 
 public class App {
-	
-	/*private char[] s;
-	{
-		s=new char[26];
-	}
-	
-	public char[] createArray()
-	{
-		for(int i=0;i<s.length;i++)
-		{
-			s[i]=(char)('A'+i);
-		}
-		return s;
-	}*/
-	
-	private static Scanner scanner=new Scanner(System.in); 
-
+	private static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		/*
-		 * //orphan object char[] str=new App().createArray(); //Enhanced for loop
-		 * for(char s:str) { System.out.println(s); }
-		 * System.out.println("======================"); //for loop for(int
-		 * i=0;i<str.length;i++) { System.out.println(str[i]); }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 */
-		
-		
-		int choice=0;
-		MyStack myStack=new MyStack();
-		System.out.print("Enter the length of the Stack:  ");
-		int length=scanner.nextInt();
-		myStack.createStack(length);
-		do {
-			System.out.println("1. Push.");
-			System.out.println("2. Pop.");
-			System.out.println("0. Exit");
-			System.out.print("enter your choice");
-			choice=scanner.nextInt();
-			switch (choice) {
-			case 1:
-				System.out.print("provide an integer value to push.");
-				int value=scanner.nextInt();
-				myStack.push(value);
-				break;
-			case 2:
-				myStack.pop();
-				break;
-			case 0:
-				System.out.println("exiting from the system. Bye .........!");
-				System.exit(0);
-
-			default:
-				System.out.println("invalid choice");
-				break;
-			}
+		int ch;
+		MyQueue mq = new MyQueue();
+		System.out.println("Enter the size for queue...");
+		int len = scanner.nextInt();
+		mq.createQueue(len);
+		do
+		{
+		System.out.println("1. Push");
+		System.out.println("2.Pop");
+		System.out.println("0. Exit");
+		System.out.println("Enter the choice...");
+		ch = scanner.nextInt();
+		switch(ch) {
+		case 1: 
+			System.out.println("Enter the integer value to push");
+			int val = scanner.nextInt();
+			mq.push(val);
+			break;
 			
-		} while (choice !=0);
+		case 2:
+			mq.pop();
+			break;
+		case 0:
+			System.out.println("Exiting....Bye Bye!!!");
+			System.exit(0);
+			break;
+			
+		default:
+			System.out.println("Invalid input");
 		
+		}
 		
-		
-		
-		
-		
-		
+		}while(ch!=0);
 
 	}
 
