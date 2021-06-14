6374bf444dbf647adc4eaefc95cad17efdfb5dc6
001ZMY744 <001ZMY744@LAPTOP-EGSTOHKO>
2021-06-14 14:18:23 -0700

    Added exception

---
+++ 14-06-21/src/comm/example/app/InvalidSalaryException.java
@@ -0,0 +1,15 @@
+package comm.example.app;
+
+@SuppressWarnings("serial")
+public class InvalidSalaryException extends RuntimeException {
+	private String message;
+
+	public InvalidSalaryException(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
