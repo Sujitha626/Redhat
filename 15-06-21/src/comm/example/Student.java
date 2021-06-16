69426c37e971ce0552b77a4fc295d8e9bb57fb00
001ZMY744 <001ZMY744@LAPTOP-EGSTOHKO>
2021-06-16 09:03:39 +0530

    added lombok

--- 15-06-21/src/comm/example/Student.java
+++ 15-06-21/src/comm/example/Student.java
@@ -1,5 +1,16 @@
 package comm.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Student implements Comparable<Object>{
 	
 	private String studentId;
