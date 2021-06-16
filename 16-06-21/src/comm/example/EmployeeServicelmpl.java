5597e6c55029b9e3a1e05a657341c1e8eeaf6d09
001ZMY744 <001ZMY744@LAPTOP-EGSTOHKO>
2021-06-16 11:41:20 +0530

    Treeset

---
+++ 16-06-21/src/comm/example/EmployeeServicelmpl.java
@@ -0,0 +1,11 @@
+package comm.example;
+
+public class EmployeeServicelmpl implements EmployeeService{
+
+	@Override
+	public void createEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
