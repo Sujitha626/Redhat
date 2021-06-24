bec44f24e601ffb378ea7fbc8a26e08698d7b4cb
001ZMY744 <001ZMY744@LAPTOP-EGSTOHKO>
2021-06-24 17:40:17 +0530

    commit

--- springcoredemo/src/main/java/org/example/demo/App.java
+++ springcoredemo/src/main/java/org/example/demo/App.java
@@ -1,5 +1,7 @@
 package org.example.demo;
 
+import org.example.demo.model.Coach;
+import org.example.demo.model.Customer;
 import org.example.demo.model.InternationalCD;
 import org.example.demo.model.SpecialEditionCD;
 import org.example.demo.model.Coach;
@@ -41,8 +43,10 @@ public class App
 			//System.out.println(cd.getDetails());
 			//SpecialEditionCD cd1=context.getBean("theCD1",SpecialEditionCD.class);
 			//System.out.println(cd1.getDetails());
-			MyInterface cd=context.getBean("theCD", MyInterface.class);
-			System.out.println(cd.getDetails());
+			//MyInterface cd=context.getBean("theCD", MyInterface.class);
+			//System.out.println(cd.getDetails());
+         Customer customer=context.getBean("theCustomer",Customer.class);
+         System.out.println(customer);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
